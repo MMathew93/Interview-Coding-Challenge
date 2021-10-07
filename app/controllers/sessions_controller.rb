fb51b29b64e63d55db0e537aa5e3db498af56b3d
Mathew McKinney <mckiney.m93@gmail.com>
2021-10-06 19:16:39 -0700

    Added the sessions controller, added login and log out functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    # find the existing user, check to see if the user can be authenticated
+    if user.present? && user.authenticate(params[:password])
+      # set up a user.id session
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Logged in successfully'
+    else
+      flash.now[:alert] = 'Invalid email or password'
+      render :new
+    end
   end
 
   def destroy
+    # delete the user session and log out
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Logged Out'
   end
 end

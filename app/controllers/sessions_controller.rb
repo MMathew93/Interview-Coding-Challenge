e53cedca863c831dcf7fbc1fffdfb211e58b3928
Mathew McKinney <mckiney.m93@gmail.com>
2021-10-06 19:06:08 -0700

    Set up the user db, set up general routes to implement, started registration controller to direct creating a new user on signup

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def home
+  end
+
+  def login
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root 'sessions#home'
+
+  get 'signup', to: 'registrations#new'
+  post 'signup', to: 'registrations#create'
+  get 'login', to: 'sessions#login'
+  post 'login', to: 'sessions#create', as: 'log_in'
+  delete 'logout', to: 'sessions#destroy'
+  get 'password', to: 'passwords#edit', as: 'edit_password'
+  patch 'password', to: 'passwords#update'
+  get 'password/reset', to: 'password_reset#new'
+  post 'password/reset', to: 'password_reset#create'
+  get 'password/reset/edit', to: 'password_resets#edit'
+  patch 'password/reset/edit', to: 'password_resets#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

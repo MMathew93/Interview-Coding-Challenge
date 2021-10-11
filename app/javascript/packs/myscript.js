07ede836fa35ee713933534706ec2d6d5ef877be
Mathew McKinney <mckiney.m93@gmail.com>
2021-10-11 13:44:57 -0700

    Added view option to click on note and display modal rather than redirecting to edit only

--- app/javascript/packs/myscript.js
+++ app/javascript/packs/myscript.js
@@ -27,3 +27,35 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 });
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const $note_container = Array.prototype.slice.call(document.querySelectorAll('.note_container'), 0);
+  if ($note_container.length > 0) {
+    $note_container.forEach( el => {
+      el.addEventListener('click', () => {
+
+        const target = el.nextElementSibling;
+
+        target.classList.toggle('is-active');
+
+      });
+    });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const $modal_close = Array.prototype.slice.call(document.querySelectorAll('.modal-close'), 0);
+  if ($modal_close.length > 0) {
+    $modal_close.forEach( el => {
+      el.addEventListener('click', () => {
+
+        const target = el.parentNode;
+
+        target.classList.remove('is-active');
+
+      });
+    });
+  }
+});
